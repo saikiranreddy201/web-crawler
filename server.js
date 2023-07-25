4139509e6b3ea20527262f691c097929b0346ac1
saikiranreddy201 <saikiranreddy2021@gmail.com>
2023-07-25 20:02:42 +0530

    Added UI to the backend

--- server.js
+++ server.js
@@ -1,43 +1,69 @@
-var Crawler = require("simplecrawler");
-var crawler = new Crawler(process.argv[2]);
-var fs = require("node-fs");
-var path = require("path");
-var url = require("url");
+const express = require("express");
+const Crawler = require("simplecrawler");
+const fs = require("node-fs");
+const path = require("path");
+const url = require("url");
 
-crawler.on("crawlstart", function() {
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.post("/startCrawler", (req, res) => {
+  const crawlUrl = req.body.crawlUrl;
+  if (!crawlUrl) {
+    return res.status(400).json({ error: "Please provide a valid URL." });
+  }
+
+  const crawler = new Crawler(crawlUrl);
+
+  crawler.on("crawlstart", function () {
+    console.log(__dirname)
     console.log("Crawl starting");
-});
+    res.json({ message: "Crawling has started." });
+  });
 
-crawler.on("fetchstart", function(queueItem) {
+  crawler.on("fetchstart", function (queueItem) {
     console.log("fetchStart", queueItem);
-});
+  });
 
-crawler.on("fetchcomplete", function(queueItem, responseBuffer) {
-    var domain = url.parse(process.argv[2]).hostname;
-    var outputDirectory = path.join(__dirname, domain);
+  crawler.on("fetchcomplete", function (queueItem, responseBuffer) {
+    var domain = url.parse(crawlUrl).hostname;
+    var outputDirectory = path.join(__dirname, "public", domain);
     var parsed = url.parse(queueItem.url);
     if (parsed.pathname === "/") {
-        parsed.pathname = "/index.html";
+      parsed.pathname = "/index.html";
     }
     var dirname = outputDirectory + parsed.pathname.replace(/\/[^\/]+$/, "");
     var filepath = outputDirectory + parsed.pathname;
     // Check if DIR exists
-    fs.exists(dirname, function(exists) {
-        // If DIR exists, write file
-        if (exists) {
-            fs.writeFile(filepath, responseBuffer, function() {});
-        } else {
-            // Else, recursively create dir using node-fs, then write file
-            fs.mkdir(dirname, 0755, true, function() {
-                fs.writeFile(filepath, responseBuffer, function() {});
-            });
-        }
+    fs.exists(dirname, function (exists) {
+      // If DIR exists, write file
+      if (exists) {
+        fs.writeFile(filepath, responseBuffer, function () {});
+      } else {
+        // Else, recursively create dir using node-fs, then write file
+        fs.mkdir(dirname, 0o755, true, function () {
+          fs.writeFile(filepath, responseBuffer, function () {});
+        });
+      }
     });
-});
+  });
 
-crawler.on("complete", function() {
+  crawler.on("complete", function () {
     console.log("Finished!");
+    res.send("Crawling completed!");
+  });
+
+  crawler.start();
 });
 
-
-crawler.start();
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
